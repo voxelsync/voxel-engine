7f8bb6d4dce7b2703f08c20b08bda04b62127521
LeyCM <cm.ley.cm@gmail.com>
2025-06-17 16:01:12 +0200

    Add first classes

---
+++ api/src/main/java/sync/voxel/api/config/VoConfig.java
@@ -0,0 +1,13 @@
+/**
+ * VOXEL-LICENSE NOTICE
+ * <br><br>
+ * This software is part of VoxelSync under the Voxel Public License. <br>
+ * Source at: <a href="https://github.com/voxelsync/voxel/blob/main/LICENSE">GITHUB</a>
+ * <br><br>
+ * Copyright (c) Ley <cm.ley.cm@gmail.com> <br>
+ * Copyright (c) contributors
+ */
+package sync.voxel.api.config;
+
+public interface VoConfig {
+}
---
+++ paper/src/main/java/sync/voxel/paper/item/VoxelMaterial.java
@@ -0,0 +1,15 @@
+/**
+ * VOXEL-LICENSE NOTICE
+ * <br><br>
+ * This software is part of VoxelSync under the Voxel Public License. <br>
+ * Source at: <a href="https://github.com/voxelsync/voxel/blob/main/LICENSE">GITHUB</a>
+ * <br><br>
+ * Copyright (c) Ley <cm.ley.cm@gmail.com> <br>
+ * Copyright (c) contributors
+ */
+package sync.voxel.paper.item;
+
+import sync.voxel.api.item.VoMaterial;
+
+public class VoxelMaterial implements VoMaterial {
+}
