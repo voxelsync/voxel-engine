064f8c640f3f936a9e3c9e981818f4cf8455d5ca
LeyCM <cm.ley.cm@gmail.com>
2025-06-22 13:01:20 +0200

    update VoRenderTypes

---
+++ voxel-api/src/main/java/sync/voxel/api/common/VoRenderType.java
@@ -0,0 +1,11 @@
+package sync.voxel.api.common;
+
+public enum VoRenderType {
+    ITEM_MODEL_ID,
+    ITEM_TEXTURE_ID,
+    BLOCK_MODEL_ID,
+    BLOCK_TEXTURE_ID,
+    ARMOR_COLOR,
+    ARMOR_MODEL, // only head and body support
+    TRIDENT;
+}
