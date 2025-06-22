3e587465ad282f8c05eb2f26400c51b51c21f32c
GamingJack3019 <gamingjack.de@outlook.de>
2025-06-22 17:58:57 +0200

    Anvile Prepare Event

--- voxel-paper/src/main/java/sync/voxel/paper/runtime/behavior/EnchantmentBehavior.java
+++ voxel-paper/src/main/java/sync/voxel/paper/runtime/behavior/EnchantmentBehavior.java
@@ -1,7 +1,10 @@
 package sync.voxel.paper.runtime.behavior;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
@@ -10,6 +13,7 @@ import sync.voxel.api.enchantment.VoEnchantment;
 import sync.voxel.paper.PaperPlugin;
 import sync.voxel.paper.runtime.enchantment.VoxelEnchantment;
 
+import javax.crypto.KEM;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,22 +33,34 @@ public final class EnchantmentBehavior implements Listener {
         ItemStack firstItem = event.getInventory().getItem(0);
         ItemStack secondItem = event.getInventory().getItem(1);
         ItemStack result = event.getResult();
+        if ((firstItem.getType() == secondItem.getType()) || (secondItem.getType() == Material.ENCHANTED_BOOK)) {
+            event.setResult(null);
+            return;
+        }
         for (VoEnchantment voEnchant : VoxelEnchantment.values()) {
+            if (!EnchantmentTarget.valueOf(voEnchant.getAttribute("enchantment_target", String.class, "tool")).includes(firstItem)) {
+                continue;
+            }
             if (voxEnchantManager.hasEnchant(firstItem, voEnchant) && voxEnchantManager.hasEnchant(secondItem, voEnchant)) {
                 int l1 = voxEnchantManager.getEnchantLevel(firstItem, voEnchant);
                 int l2 = voxEnchantManager.getEnchantLevel(secondItem, voEnchant);
-                voxEnchantManager.addEnchant(result, voEnchant, combineEnchantLevel(l1, l2));
+                voxEnchantManager.addEnchant(result, voEnchant, combineEnchantLevel(l1, l2, voEnchant.getAttribute("max_level", Integer.class, 1)));
                 continue;
             }
             if (!voxEnchantManager.hasEnchant(firstItem, voEnchant) && voxEnchantManager.hasEnchant(secondItem, voEnchant)) {
-                for (Object object : voEnchant.getAttribute("incompatible_enchants", List.class, new ArrayList<>())) {
-
+                for (String enchant : (List<String>) voEnchant.getAttribute("incompatible_enchants", List.class, new ArrayList<>())) {
+                    if (voxEnchantManager.hasEnchant(firstItem, VoxelEnchantment.valueOf(enchant))) {
+                        event.setResult(null);
+                        return;
+                    }
                 }
+                voxEnchantManager.addEnchant(result, voEnchant, voxEnchantManager.getEnchantLevel(secondItem, voEnchant));
             }
         }
+        event.setResult(result);
     }
 
-    private Integer combineEnchantLevel(Integer l1, Integer l2) {
+    private Integer combineEnchantLevel(Integer l1, Integer l2, Integer maxLevel) {
         if (l1 > l2) {
             return l1;
         }
@@ -52,7 +68,7 @@ public final class EnchantmentBehavior implements Listener {
             return l2;
         }
         else if (l2 == l1) {
-            return l1+1;
+            return l1+1 <= maxLevel ? l1+1 : l1;
         }
         else {
             return l1;
